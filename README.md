452bfc155cd71a88416bf20dfd3e696afe5b934a
PhongDien19 <phongdien1905@gmail.com>
2026-05-08 12:07:50 +0700

    update1

---
+++ README.md
@@ -0,0 +1,3 @@
+# frontend_doan
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/frontend_doan/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.frontend_doan
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
